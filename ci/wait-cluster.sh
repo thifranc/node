fd94c44cdfa407123eb808bc7ad66238e1a22020
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-11-12 16:47:42 +0200

    wait arbitrary while before running rest of tests

--- ci/wait-cluster.sh
+++ ci/wait-cluster.sh
@@ -4,12 +4,14 @@ cd /opt/node
 sudo chown -R vagrant: .
 mkdir volumes
 mkdir collections
-sudo pip3 install --system pipenv
+sudo pip3 install pipenv
 pipenv install
 
 echo "Waiting for Docker..."
-until docker version; do sleep 1; done
+until docker version; do sleep 2; done
 
 echo "Waiting for cluster autovault..."
-docker exec cluster ./cluster.py wait
+sleep 10
+docker ps
+docker exec cluster ./cluster.py wait || docker exec cluster ./cluster.py wait
 echo "Cluster provision done."
